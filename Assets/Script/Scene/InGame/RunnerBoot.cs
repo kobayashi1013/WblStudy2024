cd9d65fcb55e8a6e55a3c48f19300ee87ed66538
kohasuke <110949581+kobayashi1013@users.noreply.github.com>
2024-11-06 18:55:35 +0900

    ~17

---
+++ Assets/Script/Scene/InGame/RunnerBoot.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Fusion;
+
+namespace Scene.InGame
+{
+    public class RunnerBoot : MonoBehaviour
+    {
+        [SerializeField] private NetworkRunner _runnerPrefab; //Runnerのプレハブ
+
+        private async void Start()
+        {
+            var runner = Instantiate(_runnerPrefab); //Runnerを起動。
+            runner.ProvideInput = true; //InputAuthorityを与えるように設定する。
+
+            var startGameArgs = new StartGameArgs() //セッションの設定をする。
+            {
+                GameMode = GameMode.AutoHostOrClient, //最初に入るプレイヤーならホストとなる。
+                SceneManager = runner.GetComponent<NetworkSceneManagerDefault>(), //シーン遷移をするコンポーネント
+                PlayerCount = 2, //プレイヤーの最大人数
+            };
+
+            await runner.StartGame(startGameArgs); //セッション開始
+
+            Destroy(this.gameObject); //不要になったので削除
+        }
+    }
+}
